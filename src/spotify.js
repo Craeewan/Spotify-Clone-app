7330b8c06afe15fd233201de67ea43b75e912d36
Crae Ewan <cewan28@gmail.com>
2020-11-15 22:06:22 -0500

    final deploy

--- src/spotify.js
+++ src/spotify.js
@@ -1,35 +1,27 @@
-export const authEndPoint = 
-"https://accounts.spotify.com/authorize";
+export const authEndPoint = "https://accounts.spotify.com/authorize";
 
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://spotify-clone-app-1b53f.web.app";
 
 const clientId = "701d978a85cb433cb587396d8efc3b95";
 
-const scopes = [
-    "user-read-currently-playing",
-    "user-read-currently-playing",
-    "user-read-playback-state",
-    "user-modify-playback-state"
-];
-
+const scopes = ["user-read-currently-playing", "user-read-currently-playing", "user-read-playback-state", "user-modify-playback-state"];
 
 export const getTokenFromUrl = () => {
     console.log(window.location.hash)
 
-    return window.location.hash
-    .substring(1)
-    .split('&')
-    .reduce((initial, item) => {
-        var parts =item.split('=');
-        initial[parts[0]] = 
-        decodeURIComponent(parts[1]);
+    return window
+        .location
+        .hash
+        .substring(1)
+        .split('&')
+        .reduce((initial, item) => {
+            var parts = item.split('=');
+            initial[parts[0]] = decodeURIComponent(parts[1]);
 
             return initial;
-            
-    }, {})
+
+        }, {})
 }
-
-
 
 export const loginUrl = `${authEndPoint}
 ?client_id=${clientId}&redirect_uri=${redirectUri}
